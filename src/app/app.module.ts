8de97126ca106e1eb981c323ac23c52311189f75
james ross <james_ross@outlook.fr>
2019-12-12 15:01:57 +0100

    bad bg image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { OptionsComponent } from './options/options.component';
+import { ContactComponent } from './options/contact/contact.component';
+import { HoursComponent } from './options/hours/hours.component';
+import { ServicesComponent } from './options/services/services.component';
+import { MapComponent } from './options/map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    OptionsComponent,
+    ContactComponent,
+    HoursComponent,
+    ServicesComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule
